ee5468e7e5e18e72159f8b112edb3ac697f43100
Bozena <86227296+pakalnute@users.noreply.github.com>
2021-10-25 01:09:18 +0300

    groupGoHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FieldAnimal from "./components/FieldAnimal";
+import idGenerator from "./Common/idGenerator";
+
 
 function App() {
-    const [field, setField] = useState([]);
-    const [fieldNamber, setFieldNamber] = useState([1]);
 
+    const [field, setField] = useState([]);
+    const [fieldNamber, setFieldNamber] = useState(1);
 
     const add = (what) => {
         const fieldCopy = field.slice();
-        fieldCopy.push({animal:what, field:parsenInt(fieldNamber)});
+        fieldCopy.push({
+            id: idGenerator(),
+            animal: what,
+            field: parseInt(fieldNamber)
+        });
         setField(fieldCopy);
-        console.log(fieldCopy);
-    };
+        localStorage.setItem('animals', JSON.stringify(fieldCopy))
+    }
 
     const selectField = e => {
         setFieldNamber(e.target.value);
     }
 
-      
+    const goHome = (id) => {
+        const fieldCopy = field.slice();
+        const ind = fieldCopy.findIndex(e => e.id === id);
+        fieldCopy.splice(ind, 1);
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy))
+    }
+
+    const groupGoHome = (group) => {
+        const fieldCopy = field.slice();
+        while(true) {
+            const ind = fieldCopy.findIndex(e => e.animal === group);
+            if (ind < 0) {
+                break;
+            }
+            fieldCopy.splice(ind, 1);
+        }
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy))
+    }
+
+    useEffect(() => {
+        const animalsFromStorage = localStorage.getItem('animals');
+        if (null !== animalsFromStorage) {
+            setField(JSON.parse(animalsFromStorage));
+        }
+    }, []);
+
+
 
     return (
         <>
-        <div className="field">
-            <div className="field__part">
-                {field.map((fieldAnimal, i) => (<FieldAnimal key={i} field={1} fieldAnimal={fieldAnimal}></FieldAnimal>))}
+            <div className="field">
+                <div className="field__part">
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={1} fieldAnimal={fieldAnimal} goHome={goHome}></FieldAnimal>)}
+                </div>
+                <div className="field__part">
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={2} fieldAnimal={fieldAnimal} goHome={goHome}></FieldAnimal>)}
+                </div>
+                <div className="field__part">
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={3} fieldAnimal={fieldAnimal} goHome={goHome}></FieldAnimal>)}
+                </div>
             </div>
-            <div className="field__part">
-                {field.map((fieldAnimal, i) => (<FieldAnimal key={i} field={2} fieldAnimal={fieldAnimal}></FieldAnimal>))}
+            <div className="buttons-holder">
+                <button onClick={() => add('cow')}>Add cow</button>
+                <button onClick={() => add('sheep')}>Add sheep</button>
+                <button onClick={() => add('horse')}>Add horse</button>
+                <select value={fieldNamber} onChange={selectField}>
+                    <option value={1}>Field One</option>
+                    <option value={2}>Field Two</option>
+                    <option value={3}>Field Three</option>
+                </select>
             </div>
-            <div className="field__part">
-                {field.map((fieldAnimal, i) => (<FieldAnimal key={i} field={3} fieldAnimal={fieldAnimal}></FieldAnimal>))}
-            </div>
-
-        </div>
-        <div className="button-holder">
-            <button onClick={() => add("cow")}>Add cow</button>
-            <button onClick={() => add("sheep")}>Add sheep</button>
-            <button onClick={() => add("horse")}>Add horse</button>
-            <select value={fieldNamber} onChange={selectField}>
-                <option value={1}>Field One</option>
-                <option value={2}>Field Two</option>
-                <option value={3}>Field Three</option>
-            </select>
+            <div className="buttons-holder">
+                <button onClick={() => groupGoHome('cow')}>Go cows</button>
+                <button onClick={() => groupGoHome('sheep')}>Go sheeps</button>
+                <button onClick={() => groupGoHome('horse')}>Go horses</button>
             </div>
         </>
     );
+
 }
 
 export default App;
--- src/components/FieldAnimal.jsx
+++ src/components/FieldAnimal.jsx
@@ -1,22 +1,28 @@
 
-function FieldAnimal({ fieldAnimal, field}) {
+function FieldAnimal({ fieldAnimal, field, goHome }) {
 
 
     if (field === fieldAnimal.field && 'cow' === fieldAnimal.animal) {
         return (
             <div className="cow">
+                <h2>{fieldAnimal.id}</h2>
+                <button onClick={() => goHome(fieldAnimal.id)}>Go home</button>
             </div>
         );
     }
     else if (field === fieldAnimal.field && 'sheep' === fieldAnimal.animal) {
         return (
             <div className="sheep">
+                <h2>{fieldAnimal.id}</h2>
+                <button onClick={() => goHome(fieldAnimal.id)}>Go home</button>
             </div>
         );
     }
     else if (field === fieldAnimal.field && 'horse' === fieldAnimal.animal) {
         return (
             <div className="horse">
+                <h2>{fieldAnimal.id}</h2>
+                <button onClick={() => goHome(fieldAnimal.id)}>Go home</button>
             </div>
         );
     }
