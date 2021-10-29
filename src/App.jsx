55af2887a25bda15bcf4287bb654c2024f64c579
Bozena <86227296+pakalnute@users.noreply.github.com>
2021-10-29 19:08:13 +0300

    App.jsx zoo_ db

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,71 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ZooCreate from "./components/ZooCreate";
 import ZooList from "./components/ZooList";
+import ZooModal from "./components/ZooModal";
 function App() {
 
 
-    const[animals, setAnimals] = useState([]);
+    const [animals, setAnimals] = useState([]);
+    const [lastUpdate, setLastUpdate] = useState(Date.now())
+    const [showModal, setShowModal] = useState(false)
+    const [modalAnimal, setModalAnimal] = useState({
+        name: '',
+        type: '',
+        weight: '',
+        born: ''
+    })
+
 
     useEffect(() => {
         axios.get('http://localhost:3003/animals')
-        .then(res => {
-            setAnimals(res.data);
-            console.log(res.data);
-        })
-    }, [])
+            .then(res => {
+                setAnimals(res.data);
+                console.log(res.data);
+            })
+    }, [lastUpdate])
+
+    const create = animal => {
+        axios.post('http://localhost:3003/animals', animal)
+            .then(res => {
+                console.log(res.data);
+                setLastUpdate(Date.now());
+            })
+    }
+
+    const edit = (animal, id) => {
+        setShowModal(false);
+        axios.put('http://localhost:3003/animals/'+id, animal)
+            .then(res => {
+                console.log(res.data);
+                setLastUpdate(Date.now());
+            })
+    }
+
+    const remove = (id) => {
+        setShowModal(false);
+        axios.delete('http://localhost:3003/animals/'+id)
+            .then(res => {
+                console.log(res.data);
+                setLastUpdate(Date.now());
+            })
+    }
+
+
+    const modal = (animal) => {
+        setShowModal(true);
+        setModalAnimal(animal);
+    }
+
+    const hide = () => {
+        setShowModal(false);
+    }
 
     return (
         <div className="zoo">
-            <ZooList animals={animals}></ZooList>
+            <ZooCreate create={create}></ZooCreate>
+            <ZooList animals={animals} modal={modal}></ZooList>
+            <ZooModal edit={edit} remove={remove} hide={hide} animal={modalAnimal} showModal={showModal}></ZooModal>
         </div>
     )
 }
